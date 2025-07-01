d70f3cf4f70252c19f8f12533ac508319cdc8b43
Pratham Jain <2muchappy420@gmail.com>
2025-07-02 00:42:21 +0530

    num_members; channel serializer and multiple admins feature added

--- verbose/server/views.py
+++ verbose/server/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 from .models import Server
 from .serializer import ServerSerializer
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError, AuthenticationFailed
+from django.db.models import Count
 
 class ServerListViewSet(viewsets.ViewSet):
     queryset = Server.objects.all()
@@ -10,12 +12,33 @@ class ServerListViewSet(viewsets.ViewSet):
     def list(self, request):
         category = request.query_params.get("category")
         qty = request.query_param.get("qty")
+        by_user = request.query_params.get("by_user") == "true"
+        by_serverid = request.query_params.get("by_serverid")
+        with_num_members = request.query_params.get("with_num_members") == "true"
+
+        if by_user and not request.user.is_authenticated:
+            raise AuthenticationFailed()
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
 
-        if category:
+        if by_user:
+            user_id = request.user.id
+            self.queryset = self.queryset.filter(member=user_id)
+
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
+
+        if qty:
             self.queryset = self.queryset[: int(qty)]
 
-        serializer = ServerSerializer(self.queryset, many=True)
+        if by_serverid:
+            try:
+                self.queryset = self.queryset.filter(id=by_serverid)
+                if not self.queryset.exists():
+                    raise ValidationError(detail=f"Server with id {by_serverid} not found")
+            except ValueError:
+                raise ValidationError(detail=f"Server with id {by_serverid} not found")
+
+        serializer = ServerSerializer(self.queryset, many=True, context={"num_members": with_num_members})
         return Response(serializer.data)
