fe9ab97a2ac103e5c3b74446591e13bc05043387
Pratham Jain <2muchappy420@gmail.com>
2025-07-13 18:37:14 +0530

    drf spectator and icon, banner handling done

--- verbose/server/views.py
+++ verbose/server/views.py
@@ -5,10 +5,12 @@ from .serializer import ServerSerializer
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError, AuthenticationFailed
 from django.db.models import Count
+from .schema import server_list_docs
 
 class ServerListViewSet(viewsets.ViewSet):
     queryset = Server.objects.all()
 
+    @server_list_docs
     def list(self, request):
         category = request.query_params.get("category")
         qty = request.query_param.get("qty")
@@ -16,15 +18,18 @@ class ServerListViewSet(viewsets.ViewSet):
         by_serverid = request.query_params.get("by_serverid")
         with_num_members = request.query_params.get("with_num_members") == "true"
 
-        if by_user and not request.user.is_authenticated:
-            raise AuthenticationFailed()
+        # if by_user and not request.user.is_authenticated:
+        #     raise AuthenticationFailed()
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
 
         if by_user:
-            user_id = request.user.id
-            self.queryset = self.queryset.filter(member=user_id)
+            if by_user and request.user.is_authenticated:
+                user_id = request.user.id
+                self.queryset = self.queryset.filter(member=user_id)
+            else:
+                raise AuthenticationFailed()
 
         if with_num_members:
             self.queryset = self.queryset.annotate(num_members=Count("member"))
@@ -33,6 +38,8 @@ class ServerListViewSet(viewsets.ViewSet):
             self.queryset = self.queryset[: int(qty)]
 
         if by_serverid:
+            if not request.user.is_authenticated:
+                raise AuthenticationFailed()
             try:
                 self.queryset = self.queryset.filter(id=by_serverid)
                 if not self.queryset.exists():
